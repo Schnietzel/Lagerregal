6ac7527da9efc11013afce56f7b5e6cb4a012329
Schnietzel <mryoced@gmail.com>
2018-12-06 15:56:40 +0100

    freitag

--- src/Controller/Dateiverwaltung.java
+++ src/Controller/Dateiverwaltung.java
@@ -1,16 +1,28 @@
 package Controller;
 
 import java.io.File;
+import java.util.ArrayList;
+import Model.Lager;
 
 public class Dateiverwaltung
 {
-	public void ladeZustand(File datei)
+	public static ArrayList<Lager> ladeZustand(File datei)
 	{
 		
 	}
 	
-	public void speicherZustand(File datei)
+	public static void speicherZustand(File datei, ArrayList<Lager> lager)
 	{
+		StringBuilder csv = new StringBuilder();
 		
+		
+	}
+	
+	private String getLagerRecursive(StringBuilder sb, ArrayList<Lager> lager, int counter, int depth)
+	{
+		if (lager.isEmpty())
+		{
+			String ret = 
+		}
 	}
 }
--- src/Controller/Lagerverwaltung.java
+++ src/Controller/Lagerverwaltung.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import java.io.File;
 import java.util.ArrayList;
 import Model.Lager;
 
@@ -12,10 +13,49 @@ public class Lagerverwaltung
 		lager = new ArrayList<Lager>();
 		
 		initTestlager();
+		
+		String datei = "C:\\Test\\test.csv";
+		File file = new File(datei);
+		Dateiverwaltung.speicherZustand(file, lager);
 	}
 
 	private static void initTestlager()
 	{
-		Lager lager = new Lager("Hannover-Misburg", 10, 10);
+		ArrayList<Lager> lNds = new ArrayList<Lager>();
+		ArrayList<Lager> lDeutschland = new ArrayList<Lager>();
+		ArrayList<Lager> lFrankreich = new ArrayList<Lager>();
+		ArrayList<Lager> lEuropa = new ArrayList<Lager>();
+		
+		Lager Hannover = new Lager("Hannover", 10, 10);
+		Lager Nienburg = new Lager("Nienburg", 10, 10);
+		lNds.add(Hannover);
+		lNds.add(Nienburg);
+		
+		Lager Niedersachsen = new Lager("Niedersachsen", lNds);
+		Lager NRW = new Lager("NRW", 10, 10);
+		Lager Bremen = new Lager("Bremen", 10, 10);
+		Lager Hessen = new Lager("Hessen", 10, 10);
+		lDeutschland.add(Niedersachsen);
+		lDeutschland.add(NRW);
+		lDeutschland.add(Bremen);
+		lDeutschland.add(Hessen);
+		
+		Lager Deutschland = new Lager("Deutschland", lDeutschland);
+		
+		Lager Paris = new Lager("Paris", 10, 10);
+		Lager Orleans = new Lager("Orleans", 10, 10);
+		lFrankreich.add(Paris);
+		lFrankreich.add(Orleans);
+		
+		
+		Lager Frankreich = new Lager("Frankreich", lFrankreich);
+		Lager England = new Lager("England", 10, 10);
+		lEuropa.add(Frankreich);
+		lEuropa.add(England);
+		
+		Lager Europa = new Lager("Europa", lEuropa);
+		
+		lager.add(Deutschland);
+		lager.add(Europa);
 	}
 }

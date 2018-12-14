0033817e6513f4d8dd34f1bcc6a02bda088d9ca5
Schnietzel <mryoced@gmail.com>
2018-12-14 13:56:15 +0100

    AStuff

--- src/Model/Buchung.java
+++ src/Model/Buchung.java
@@ -1,5 +1,13 @@
 package Model;
 
-public class Buchung {
-
+public class Buchung 
+{
+	private Lager ziellager;
+	private int prozent;
+	
+	public Buchung(Lager ziellager, int prozent)
+	{
+		this.ziellager = ziellager;
+		this.prozent = prozent;
+	}
 }

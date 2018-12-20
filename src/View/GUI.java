4bb134766e927552b7b8a135a6acb872f031872d
Swoodox <37533413+Swoodox@users.noreply.github.com>
2018-12-20 13:45:16 +0100

    So krams eben

--- src/View/GUI.java
+++ src/View/GUI.java
@@ -1,4 +1,29 @@
 package View;
 
-public class GUI {
+import mdlaf.MaterialLookAndFeel;
+
+import javax.swing.*;
+
+public class GUI extends JFrame {
+    private JPanel outerpanel;
+    private JTabbedPane tabs;
+    private JTree tree1;
+    private JButton undoButton;
+    private JButton redoButton;
+    private JList list1;
+    private JPanel innerpanel;
+    private JToolBar toolbar;
+
+    public GUI(){
+        this.setContentPane(outerpanel);
+        try {
+            UIManager.setLookAndFeel (UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (UnsupportedLookAndFeelException | ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+            e.printStackTrace ();
+        }
+        this.pack();
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 }

3fa138c4ab137cc95263a99a9dfc338c09c35767
Schnietzel <Schnietzel@192.168.0.10>
2018-12-23 22:07:06 +0100

    Historie gefüllt

--- src/View/HistorieTab.java
+++ src/View/HistorieTab.java
@@ -1,23 +1,31 @@
 package View;
 
 import javax.swing.*;
+
+import Controller.ControllerSingleton;
+import Controller.GUITools;
+import Model.Buchung;
+import Model.Lieferung;
+
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 class HistorieTab extends JPanel{
+    private GUITools gt;
 
     private JList hList;
-
-    HistorieTab(){
+    HistorieTab()
+    {
+    	gt = ControllerSingleton.getGTInstance();
         this.setLayout(new BorderLayout());
 
-        hList = new JList();
-        // TODO: Liste füllen
-
-        //Liste an JList
-        //hList = new JList<a>(b);
-        hList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        DefaultListModel dlm = new DefaultListModel();
+        gt.getLieferung(ControllerSingleton.getBVInstance().getHistorie(), dlm);
+        
+        
+        hList = new JList(dlm);
+        
         //JList an Scrollpane
         JScrollPane hScrollPane = new JScrollPane(hList);
         //ScrollPane an Panel für Liste
